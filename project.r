810c2c424a630e47b8066c129c443801f996c03f
Zac Stewart <zgstewart@gmail.com>
2012-07-07 00:19:47 -0400

    Fix a bunch of problems and retry first submission >:(

--- project.r
+++ project.r
@@ -18,9 +18,10 @@ add.gen.features <- function (data.set) {
 }
 
 train <- read.delim('data/train.tsv', sep="\t", header=TRUE)
+test <- read.delim('data/public_leaderboard.tsv', sep="\t", header=TRUE)
+
 train$EssayText <- as.character(train$EssayText)
-test <- read.delim('data/train.tsv', sep="\t", header=TRUE)
-test$EssayText <- as.character(train$EssayText)
+test$EssayText <- as.character(test$EssayText)
 
 #This is to add features specific to the essay set
 #train.1 <- train[which(train$EssaySet == 1),]
@@ -28,6 +29,10 @@ test$EssayText <- as.character(train$EssayText)
 train <- add.gen.features(train)
 test <- add.gen.features(test)
 
-model <- randomForest(train[,6:ncol(train)], train$Score1, sampsize=nrow(train)*.3, ntree=1000)
+model <- randomForest(train[c('Length', 'Words', 'Sentences')], train$Score1, sampsize=nrow(train)*.3, ntree=1000)
 
 predictions <- predict(model, test)
+round.predictions <- round(predictions)
+
+submission <- data.frame(id=test$Id, essay_score=round.predictions)
+write.csv(submission, file='submission.csv', row.names=FALSE)
