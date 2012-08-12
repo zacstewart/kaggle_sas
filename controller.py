1bec85a6c245b4f2e8912bff2ff87de29cfd4a2c
Zac Stewart <zgstewart@gmail.com>
2012-08-12 10:04:46 -0400

    Remove regressors
    These didn't seem to really be helping and they were making the code more clumsy
    I may add them back once I have things cleaned up

--- controller.py
+++ controller.py
@@ -23,35 +23,23 @@ if __name__ == "__main__":
     =====================
     ''' % {'set': i}
 
-    classifiers = {
-        'ncn': NearestCentroid(metric='euclidean', shrink_threshold=None),
-        'gnb': GaussianNB(),
-        'mnb': MultinomialNB(),
-        'dtc': tree.DecisionTreeClassifier(),
-        'lda': LDA(),
-        'lr': LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0,
-          fit_intercept=True, intercept_scaling=1, class_weight=None),
-        'rc': RidgeClassifier(alpha=1.0, fit_intercept=True, normalize=False,
-          copy_X=True, tol=0.001, class_weight=None),
-        'svc': svm.SVC(C=100.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
-          gamma=0.001, kernel='rbf', probability=False, shrinking=True, tol=0.001,
-          verbose=False),
-        'lsvc': svm.LinearSVC(penalty='l2', loss='l2', dual=True, tol=0.0001, C=1.0,
-          multi_class='ovr', fit_intercept=True, intercept_scaling=1,
-          class_weight=None, verbose=0),
-        'rf': RandomForestClassifier(n_estimators=150, min_samples_split=2, n_jobs=-1)}
-    regressors = {
-        'gbr': GradientBoostingRegressor(n_estimators=100, learn_rate=1.0,
-          max_depth=1, random_state=0, loss='ls'),
-        'dtr': tree.DecisionTreeRegressor(),
-        #'gp': gaussian_process.GaussianProcess(theta0=1e-2, thetaL=1e-4, thetaU=1e-1),
-        'svr': svm.SVR(C=1.0, cache_size=200, coef0=0.0, degree=3,
-          epsilon=0.1, gamma=0.5, kernel='rbf', probability=False, shrinking=True,
-          tol=0.001, verbose=False),
-        'ridge': Ridge(alpha=0.5, copy_X=True, fit_intercept=True, normalize=False, tol=0.001),
-        'knnr': KNeighborsRegressor(n_neighbors=5, weights='uniform', algorithm='auto',
-          leaf_size=30, warn_on_equidistant=True, p=2)}
-    models = dict(classifiers.items() + regressors.items())
+    models = {
+      'ncn': NearestCentroid(metric='euclidean', shrink_threshold=None),
+      'gnb': GaussianNB(),
+      'mnb': MultinomialNB(),
+      'dtc': tree.DecisionTreeClassifier(),
+      'lda': LDA(),
+      'lr': LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0,
+        fit_intercept=True, intercept_scaling=1, class_weight=None),
+      'rc': RidgeClassifier(alpha=1.0, fit_intercept=True, normalize=False,
+        copy_X=True, tol=0.001, class_weight=None),
+      'svc': svm.SVC(C=100.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+        gamma=0.001, kernel='rbf', probability=False, shrinking=True, tol=0.001,
+        verbose=False),
+      'lsvc': svm.LinearSVC(penalty='l2', loss='l2', dual=True, tol=0.0001, C=1.0,
+        multi_class='ovr', fit_intercept=True, intercept_scaling=1,
+        class_weight=None, verbose=0),
+      'rf': RandomForestClassifier(n_estimators=150, min_samples_split=2, n_jobs=-1)}
 
     # Get all essays for train+lb
     strows = essaySet(i, trows, th)
@@ -102,7 +90,6 @@ if __name__ == "__main__":
     for mname, model in models.items():
       cv_scores = []
       qwks = []
-      rmses = []
       pwidgets =  [mname, ' Fold ', Counter(), ' ', ' ', Bar(marker='=',left='[',right=']'), ' ', ETA()]
       foldbar = ProgressBar(widgets=pwidgets, maxval=k)
       foldbar.start()
@@ -124,24 +111,16 @@ if __name__ == "__main__":
         score = model.score(cx, cy)
         p = model.predict(cx)
         p = [int(pe) for pe in p]
-        if mname in classifiers:
-          qwk = quadratic_weighted_kappa(cy, p, 0, 3)
-          qwks.append(qwk)
-        else:
-          my_rmse = rmse(cy, p)
-          rmses.append(my_rmse)
+        qwk = quadratic_weighted_kappa(cy, p, 0, 3)
+        qwks.append(qwk)
         cv_scores.append(score)
       foldbar.finish()
 
       cv_mean = sum(cv_scores) / len(cv_scores)
       print '  - Mean score for %(model)s: %(score)f' % {'model': mname, 'score': cv_mean}
       scores[mname] = cv_mean
-      if mname in classifiers:
-        qwk_mean = sum(qwks) / len(qwks)
-        print '  - Mean QWK for %(model)s: %(score)f' % {'model': mname, 'score': qwk_mean}
-      if mname in regressors:
-        rmse_mean = sum(rmses) / len(rmses)
-        print '  - Mean RMSE for %(model)s: %(score)f' % {'model': mname, 'score': rmse_mean}
+      qwk_mean = sum(qwks) / len(qwks)
+      print '  - Mean QWK for %(model)s: %(score)f' % {'model': mname, 'score': qwk_mean}
 
     print 'Building super training set...'
 
@@ -153,11 +132,11 @@ if __name__ == "__main__":
 
     cv_predictions = dict()
     lb_predictions = dict()
-    supertrainhead = ['Id', 'Score1'] + classifiers.keys()
-    superlbhead = ['Id'] + classifiers.keys()
+    supertrainhead = ['Id', 'Score1'] + models.keys()
+    superlbhead = ['Id'] + models.keys()
     sth = dict(zip(supertrainhead, range(len(supertrainhead))))
     slh = dict(zip(superlbhead, range(len(superlbhead))))
-    for mname, model in classifiers.items():
+    for mname, model in models.items():
       print '  == Model: ' + mname
       print '  -- training...'
       model.fit(trainx, trainy)
@@ -171,7 +150,7 @@ if __name__ == "__main__":
       supertrainrow = [None for _ in range(len(supertrainhead))]
       supertrainrow[sth['Id']] = row[th['Id']]
       supertrainrow[sth['Score1']] = row[th['Score1']]
-      for mname in classifiers.keys():
+      for mname in models.keys():
         supertrainrow[sth[mname]] = int(cv_predictions[mname][i])
       supertrain.append(supertrainrow)
 
@@ -179,17 +158,16 @@ if __name__ == "__main__":
     for (i, row) in enumerate(lfeatures):
       superlbrow = [None for _ in range(len(superlbhead))]
       superlbrow[slh['Id']] = row[lh['Id']]
-      for mname in classifiers.keys():
+      for mname in models.keys():
         superlbrow[slh[mname]] = int(lb_predictions[mname][i])
       superlb.append(superlbrow)
 
     print 'Cross validating super model...'
     folds = kFold(supertrain, k)
     modelscores = dict()
-    for mname, model in classifiers.items():
+    for mname, model in models.items():
       superduperscores = []
       superduperqwks = []
-      superduperrmses = []
       pwidgets =  [mname, ' Fold ', Counter(), ' ', ' ', Bar(marker='=',left='[',right=']'), ' ', ETA()]
       foldbar = ProgressBar(widgets=pwidgets, maxval=k)
       foldbar.start()
@@ -208,12 +186,8 @@ if __name__ == "__main__":
         superduperscores.append(score)
         preds = model.predict(cvx)
         preds = [int(p) for p in preds]
-        if mname in classifiers:
-          qwk = quadratic_weighted_kappa(cvy, preds, 0, 3)
-          superduperqwks.append(qwk)
-        elif mname in regressors:
-          my_rmse = rmse(cvy, preds)
-          superduperrmses.append(my_rmse)
+        qwk = quadratic_weighted_kappa(cvy, preds, 0, 3)
+        superduperqwks.append(qwk)
       foldbar.finish()
       mean_superduper_score = sum(superduperscores) / len(superduperscores)
       print 'Mean super duper CV score: ' + str(mean_superduper_score)
@@ -223,7 +197,7 @@ if __name__ == "__main__":
 
     bestmodel = sorted(modelscores.iteritems(), key=operator.itemgetter(1), reverse=True)[0][0]
 
-    final_model = classifiers[bestmodel]
+    final_model = models[bestmodel]
     final_trainx = [row[2:] for row in supertrain]
     final_trainy = [row[1] for row in supertrain]
     final_lbx = [row[1:] for row in superlb]
